8de1922ec41afa4469bd7b620abb25b7e69aa04e
remy <remy.auricoste@gmail.com>
2015-09-02 12:52:25 +0200

    attempt to fix heroku deploy

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ var SocketServer = require("rauricoste-websocket-room-server");
 
 var port = process.env.PORT || 8000;
 
-var fileServer = new static.Server('.', {
+var fileServer = new static.Server('./src/main', {
+    cache: 1
+});
+var distServer = new static.Server('.', {
     cache: 1
 });
 
+String.prototype.startsWith = function(start) {
+    return start.length <= this.length && this.substring(0, start.length) === start;
+}
+
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        fileServer.serve(request, response);
+        if (request.url.startsWith("/dist/")) {
+            distServer.serve(request, response);
+        } else {
+            fileServer.serve(request, response);
+        }
     }).resume();
 }).listen(port);
 
