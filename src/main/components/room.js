cf2052cc0ebdd8cfac0cf13b2c5b865cefaac006
remy <remy.auricoste@gmail.com>
2015-09-02 09:39:01 +0200

    prepare clicks on users

--- src/main/components/room.js
+++ src/main/components/room.js
@@ -1,16 +1,38 @@
 /* import filename */ var filename = 'room';
 /* import directives */ var directives = require('./../js/directives');
+/* import Select */ var Select = require("./../js/tools/Select");
+
+var $ = Select;
 
 var KEY_ENTER = 13;
+var KEY_ESC = 27;
 
 directives.addTemplate(filename, {
     room: "="
-}, function($scope) {
+}, function($scope, elements) {
+    var el = elements[0];
+    var textEl = $(el).find(".text")[0];
+    textEl.focus();
     $scope.text = "";
     $scope.onKeydown = function(event) {
         if (event.keyCode === KEY_ENTER && $scope.text.length) {
             $scope.room.send($scope.text);
             $scope.text = "";
+        } else if (event.keyCode === KEY_ESC) {
+            $scope.text = "";
         }
+    }
+    $scope.onUserMouseOver = function(user) {
+        $scope.userMouseOver = user;
+    }
+    $scope.onUserMouseOut = function(user) {
+        $scope.userMouseOver = null;
+    }
+    $scope.onUserClick = function(user) {
+        $scope.text = user+" > "+$scope.text;
+        textEl.focus();
+    }
+    $scope.onMemberClick = function(member) {
+        // TODO open private room
     }
 });
